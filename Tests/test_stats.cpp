d087cc653c9d096debecf73f6cb79c94a3d07a4e
Benito Kestelman <bkestelman@outlook.com>
2022-11-09 22:10:02 -0500

    Add more tests for correlation

--- Tests/test_stats.cpp
+++ Tests/test_stats.cpp
@@ -21,6 +21,22 @@ TEST(TestStats, TestStandardDeviation) {
 
 TEST(TestStats, TestCorrelation) {
 	vector<float> vec1{ 1, 3, 5 };
-	vector<float> vec2{ 2, 6, 10 };
+	vector<float> vec3{ 4, 7, 7 };
 	EXPECT_NEAR(correlation(vec1, vec1), 1, 0.0001);
+	EXPECT_NEAR(correlation(vec1, vec3), 0.866, 0.0001);
+}
+
+TEST(TestStats, TestCorrelationSymmetry) {
+	vector<float> vec1{ 1, 3, 5 };
+	vector<float> vec2{ 2, 6, 10 };
+	EXPECT_NEAR(correlation(vec1, vec2), correlation(vec2, vec1), 0.0001);
+}
+
+TEST(TestStats, TestCorrelationScaleInvariance) {
+	vector<float> vec1{ 1, 3, 5 };
+	vector<float> vec2{ 4, 7, 7 };
+	vector<float> vec1_scaled{ 2, 6, 10 };
+	EXPECT_NEAR(correlation(vec1, vec2), correlation(vec1_scaled, vec2), 0.0001);
+	vector<float> vec1_shifted{ 3, 5, 7 };
+	EXPECT_NEAR(correlation(vec1, vec2), correlation(vec1_shifted, vec2), 0.0001);
 }
